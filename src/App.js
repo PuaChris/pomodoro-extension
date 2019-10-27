6ea09ce228bbcd4c6bf5dc9a7861a6779688511e
PuaChris <chris.pua@mail.utoronto.ca>
2019-10-27 16:31:42 -0400

    Replaced handler functions in App.js with a setTimer nd added a new function which changes the timer state

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
 import React, { Component } from 'react'
 import Timer from './Timer.js'
+import * as Constants from './constants'
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             minutes: 0,
-            pomodoroTime: 25,
+            pomodoroTime: 1,
             shortBreakTime: 5,
             longBreakTime: 30,
             numShortBreaks: 0,
-            timerStates: ["Pomodoro", "ShortBreak", "LongBreak"],
-            timerType: "Pomodoro",
+            // timerStates: ["Pomodoro", "ShortBreak", "LongBreak"],
+            timerType: Constants.POMODORO,
             isFinished: true
         }
-
-        // * No need to bind if using the Javascript function syntax (?)
-        // this.pomodoroHandler = this.pomodoroHandler.bind(this);
-        // this.shortBreakHandler = this.shortBreakHandler.bind(this);
-        // this.longBreakHandler = this.longBreakHandler.bind(this);
     }
 
+
+
     changeTimerType = () => {
-        if (this.state.timerType === this.state.timerStates[0] && this.state.isFinished){
-            if (this.state.numShortBreaks % 4 === 0){
-                this.setState({
-                    timerType: this.state.timerStates[2]
-                })
+        if (this.state.isFinished){
+            if (this.state.timerType === Constants.POMODORO){
+
+                // Assume 4 cycles are needed to go into long break
+                if (this.state.numShortBreaks % 4 === 0){
+                    this.setState({
+                        timerType: Constants.LONGBREAK
+                    })
+                }
+    
+                // Short Break
+                else {
+                    this.setState({
+                        timerType: Constants.SHORTBREAK
+                    })
+                }
             }
+    
+            // Must be in one of the two break states
             else {
-                this.setState({
-                    timerType: this.state.timerStates[1]
-                })
+    
             }
-        }
+    
+            this.setState({
+                isFinished: false
+            })
+        }   
     }
 
     decrementMinute = () => {
@@ -42,24 +55,39 @@ class App extends Component {
         });
     }
 
-    pomodoroHandler = () => {
-        this.setState({
-            minutes: this.state.pomodoroTime
-        });
+    setTimer = () => {
+        // Pomodoro State
+        if (this.state.timerType === Constants.POMODORO){
+            this.setState({
+                minutes: this.state.pomodoroTime
+            }); 
+        }  
         
+        // Short Break State
+        else if (this.state.timerType === Constants.SHORTBREAK){
+            this.setState({
+                minutes: this.state.shortBreakTime
+            }); 
+        }
+
+        // Long Break State
+        else if (this.state.timerType === Constants.LONGBREAK){
+            this.setState({
+                minutes: this.state.longBreakTime
+            }); 
+        }
     }
 
     render() {
         return (
             <div className="Pomodoro">
-                <Timer  minutes={this.state.minutes}
-                        decrementMinute={this.decrementMinute}
-                        pomodoroHandler={this.pomodoroHandler}
-                        timerStates={this.state.timerStates}
-                        timerType={this.state.timerType}
-                        isFinished={this.state.isFinished} 
-                        // TODO: Complete and pass on additional handlers
-                        />
+                <Timer  
+                    minutes={this.state.minutes}
+                    decrementMinute={this.decrementMinute}
+                    setTimer={this.setTimer}
+                    changeTimerType={this.changeTimerType}
+                    timerType={this.state.timerType}
+                    isFinished={this.state.isFinished} />
             </div>
         )
     }
--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import * as Constants from './constants'
 // ! Source code: https://codepen.io/jurekbarth/pen/pgYGBm
 
 class Timer extends Component {
@@ -7,44 +8,36 @@ class Timer extends Component {
     this.state = {
       seconds: "00"
     };    
-
-    if (this.props.timerType === this.props.timerStates[0]){
-      this.props.pomodoroHandler();
-    }
-    
-    // It's because of the way Javacsript handles contexts of functions.
-    // Essentially, if not calling the function directly from the object
-    // and that function is not bounded to any context, it loses the context
-    // this.tick = this.tick.bind(this);
+    this.props.setTimer();
   }
-  // componentWillUnmount = () => {
-  //   clearInterval(this.timer);
-  // }
 
   tick = () => {
     if (this.props.minutes >= 0) {
-      this.props.isFinished = false;
 
+      // Timer is finished
       if (this.props.minutes <= 0 && this.state.seconds <= 0) {
         clearInterval(this.timer);
-        this.props.isFinished = true;
+        this.props.changeTimerType();
         return;
       }
       
+      // The current minute finishes
       else if (this.state.seconds <= 0) {
         this.props.decrementMinute();
-
         this.setState({
-          seconds: 59
+          seconds: 2
         });
       }
 
+      // Case to manually add a 0 when seconds is only a single digit
       // TODO: Optimzation opportunity -> figure out a way to not manually insert a 0 in the timer
       else if (this.state.seconds < 10) {
         this.setState({ 
           seconds: "0" + (this.state.seconds - 1)
         });
       } 
+
+      // Normal timer countdown functionality
       else {
         this.setState({
            seconds: this.state.seconds - 1
@@ -66,13 +59,7 @@ class Timer extends Component {
 
   resetTimerBtn = () => {
     clearInterval(this.timer);
-    
-    if (this.props.timerType === this.props.timerStates[0]){
-      this.props.pomodoroHandler();
-    }
-
-    // TODO: Set conditionals for what state it is to set the timer (e.g. pomodoroState, etc.)
-
+    this.props.setTimer();
     this.setState({
       seconds: "00"
     });
