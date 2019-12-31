aeca90ca8f1390c105061d92c3b88bc558261bd6
Chris Pua <chris.pua@mail.utoronto.ca>
2019-12-30 23:24:50 -0500

    Incoporated save button to successfully save duration values for timer

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,58 +1,36 @@
 import React, { Component } from 'react';
 
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-// import * as firebase from "firebase/app";
-require("firebase/firestore");
-
 import { db } from '../api/Context';
+import { Context } from '../api/_context';
 
 import * as Constants from '../resources/Constants';
 import * as Helper from '../resources/Helper';
 
 import Timer from './Timer';
+import TimerConfig from './TimerConfig';
 
 export default class App extends Component{
     constructor(props){
         super(props);
-        this.UserData;
-        
-        db.collection("context").where("UserEmail", "==", Constants.USERID).get()
-        .then(QuerySnapShot => {
-            this.UserData = QuerySnapShot.docs.map(doc => doc.data());
-            console.log(this.UserData); // array of cities objects
-
-            this.setState({
-                phase: Constants.FOCUS,
-                duration:  this.UserData[0].FocusLength,
-                numPomodoros: 0
-            });
-        });
+        this.state = {
+            phase: Constants.FOCUS,
+            duration: Helper.minutesToSeconds(Constants.FOCUS_LENGTH),
+            numPomodoros: 0,
+            focusDuration: Constants.FOCUS_LENGTH,
+            shortBreakDuration: Constants.SHORTBREAK_LENGTH,
+            longBreakDuration: Constants.LONGBREAK_LENGTH
+        }
 
         this.updatePhaseToFocus = this.updatePhaseToFocus.bind(this);
         this.updatePhaseToBreak = this.updatePhaseToBreak.bind(this);
         this.updateNumPomodoros = this.updateNumPomodoros.bind(this);
-
-        db.collection("context").doc("test").set({
-            UserEmail: "testemail.com",
-            FocusLength: 25,
-            ShortBreakLength: 5,
-            LongBreakLength: 15,
-            PomodorosHistory: {
-                Date: new Date()
-            }
-        })
-        .then(function() {
-            console.log("Document successfully written!");
-        })
-        .catch(function(error) {
-            console.error("Error writing document: ", error);
-        });
+        this.updateTimerConfig = this.updateTimerConfig.bind(this);
     }
 
     updatePhaseToFocus() {
         this.setState({
             phase: Constants.FOCUS,
-            duration: Helper.minutesToSeconds(69)
+            duration: Helper.minutesToSeconds(this.state.focusDuration)
         });
     }
 
@@ -62,13 +40,13 @@ export default class App extends Component{
         if (numPomodoros % Constants.NUM_POMODOROS_BEFORE_LONGBREAK === 0 && numPomodoros > 0) {
             this.setState({
                 phase: Constants.LONGBREAK,
-                duration: Helper.minutesToSeconds(Constants.LONGBREAK_LENGTH)
+                duration: Helper.minutesToSeconds(this.state.longBreakDuration)
             });
         }
         else {
             this.setState({
                 phase: Constants.SHORTBREAK,
-                duration: Helper.minutesToSeconds(Constants.SHORTBREAK_LENGTH)
+                duration: Helper.minutesToSeconds(this.state.shortBreakDuration)
             });
         }
     }
@@ -79,19 +57,55 @@ export default class App extends Component{
         });
     }
     
+    updateTimerConfig(event) {
+        let durationValues = [...(event.target)];
+        let _focusDuration, _shortBreakDuration, _longBreakDuration
+        console.log(durationValues);
+
+        durationValues.forEach(function(element) {
+            console.log(element.id);
+            console.log(element.value)
+            if (element.type != "submit") {
+                switch (element.id) {
+                    case "focus_duration":
+                        _focusDuration = element.value;
+                        break;
+                    case "short_break_duration":
+                        _shortBreakDuration = element.value;
+                        break;
+                    case "long_break_duration":
+                        _longBreakDuration = element.value;
+                        break;
+                }
+            }
+          });
+
+        this.setState({
+            focusDuration: _focusDuration ? _focusDuration : this.state.focusDuration,
+            shortBreakDuration: _shortBreakDuration ? _shortBreakDuration : this.state.shortBreakDuration,
+            longBreakDuration: _longBreakDuration ? _longBreakDuration : this.state.longBreakDuration
+        });
+
+        
+    }
+    
     render() {
         return this.state && this.state.duration ? (
             <div className="timer-container">
-                <header>
-                    <h1>Puamodoro Timer</h1>
-                    <Timer 
-                        phase={this.state.phase}
-                        duration={this.state.duration}
-                        updatePhaseToFocus={this.updatePhaseToFocus}
-                        updatePhaseToBreak={this.updatePhaseToBreak}
-                    />
-                    <h2>Phase: {this.state.phase}</h2>
-                </header>
+                <h1>Puamodoro Timer</h1><br/>
+                <h1>Phase: {this.state.phase}</h1>
+                <Timer 
+                    phase={this.state.phase}
+                    duration={this.state.duration}
+                    updatePhaseToFocus={this.updatePhaseToFocus}
+                    updatePhaseToBreak={this.updatePhaseToBreak}
+                />
+                <TimerConfig
+                    focusDuration={this.state.focusDuration}
+                    shortBreakDuration={this.state.shortBreakDuration}
+                    longBreakDuration={this.state.longBreakDuration}
+                    updateTimerConfig={this.updateTimerConfig}
+                />
             </div>
         ) : <span>Loading...</span>;
     }
---
+++ imports/ui/TimerConfig.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class TimerConfig extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            focusDuration: this.props.focusDuration,
+            shortBreakDuration: this.props.shortBreakDuration,
+            longBreakDuration: this.props.longBreakDuration
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.updateTimerConfig(event);
+        alert('Values saved.');
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h2>Focus Value</h2>
+                <input type="text" className="input" id="focus_duration" value={this.state.focusDuration} onChange={e => this.setState({focusDuration: e.target.value})}/>
+                <br/>
+                <h2>Short Break Value</h2>
+                <input type="text" className="input" id="short_break_duration" value={this.state.shortBreakDuration} onChange={e => this.setState({shortBreakDuration: e.target.value})}/>
+                <br/>
+                <h2>Long Break Value</h2>
+                <input type="text" className="input" id="long_break_duration" value={this.state.longBreakDuration} onChange={e => this.setState({longBreakDuration: e.target.value})}/>
+                <br/>
+                <input type="submit" className="submit" value="Save"/>
+            </form>
+        );
+    }
+}
